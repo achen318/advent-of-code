870643ef1604d4170c575869b35c746a2644089c
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-12-24 19:39:44 -0500

    Day 9, 2 pointers

--- 2024/09/day09.cpp
+++ 2024/09/day09.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -65,48 +66,56 @@ ll part_2(string &disk_map)
     int id = 0;
     bool file = true;
 
-    vector<int> blocks;
+    vector<pair<int, int>> id_freq;
 
     for (char c : disk_map)
     {
         if (file)
-        {
-            for (int _ = '0'; _ < c; ++_)
-                blocks.push_back(id);
-            ++id;
-        }
+            id_freq.push_back({id++, c - '0'});
         else
-            for (int _ = '0'; _ < c; ++_)
-                blocks.push_back(-1);
+            id_freq.push_back({-1, c - '0'});
 
         file = !file;
     }
 
-    size_t free_i = 0;
-    size_t block_i = blocks.size() - 1;
-
-    while (free_i < block_i)
+    for (size_t i = 0; i < id_freq.size(); ++i)
     {
-        while (blocks[free_i] != -1)
-            ++free_i;
-        while (blocks[block_i] == -1)
-            --block_i;
+        if (id_freq[i].first != -1)
+            continue;
 
-        if (free_i < block_i)
+        for (size_t j = id_freq.size() - 1; j > i; --j)
         {
-            blocks[free_i] = blocks[block_i];
-            blocks[block_i] = -1;
-        }
+            if (id_freq[j].first == -1)
+                continue;
 
-        ++free_i;
-        --block_i;
+            if (id_freq[i].second >= id_freq[j].second)
+            {
+                if (id_freq[i].second > id_freq[j].second)
+                {
+                    id_freq.insert(id_freq.begin() + i + 1, {-1, id_freq[i].second - id_freq[j].second});
+                    ++j;
+                    id_freq[i].second = id_freq[j].second;
+                }
+
+                id_freq[i].first = id_freq[j].first;
+                id_freq[j].first = -1;
+                break;
+            }
+        }
     }
 
-    for (size_t i = 0; i < blocks.size(); ++i)
-        if (blocks[i] == -1)
-            break;
-        else
-            checksum += i * blocks[i];
+    size_t i = 0;
+
+    for (auto [id, freq] : id_freq)
+    {
+        i += freq;
+
+        if (id == -1)
+            continue;
+
+        for (int j = 0; j < freq; ++j)
+            checksum += (i - j - 1) * id;
+    }
 
     return checksum;
 }
